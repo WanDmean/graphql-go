07b80b7a58045b969b4aebaf84916959c7ff10af
Wan D Mean <ph.mean@gmail.com>
2021-05-16 16:00:38 +0700

    add config .env file

--- src/server.go
+++ src/server.go
@@ -3,21 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/WanDmean/graphql-go/graph"
 	"github.com/WanDmean/graphql-go/graph/generated"
+	"github.com/WanDmean/graphql-go/src/config"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	port := config.PORT
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
